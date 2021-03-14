854adc276fa72f00f4651f811d78c64a2b09e35d
Ezequiel Mattos <ezequielsfc@hotmail.com>
2021-03-14 18:56:57 -0300

    Projeto da lista de exercicio

---
+++ ListaExercicios/Exe01/Exe01.js
@@ -0,0 +1,7 @@
+let altura = Math.floor (Math.random() * 100);
+let largura = Math.floor (Math.random() * 100);
+let area = altura * largura;
+
+console.log("Altura: " + altura + " Foi Gerado Randomicamente");
+console.log(`Largura: ${largura} Foi Gerado Randomicamente`);
+console.log(area);
---
+++ ListaExercicios/Exe01/exemplos.js
@@ -0,0 +1,8 @@
+var variavel = 123;
+console.log(variavel);
+variavel = "123";
+console.log(variavel);
+variavel = true;
+console.log(variavel);
+variavel = {nome: "Diogo"};
+console.log(variavel);
